21311365c71e092eacf2baabcaebab553b3646f2
Lana150 <astra.altair150@gmail.com>
2025-02-14 11:20:13 +0100

    added changes

--- src/generics/3.ts
+++ src/generics/3.ts
@@ -1,6 +1,12 @@
-function merge<T extends Record<string, any>, U extends Record<string, any>>(
+function merge<T extends object, U extends object>(objA: T, objB: U): T & U {
+    return Object.assign({}, objA, objB);
+}
+
+
+
+/*function merge<T extends Record<string, any>, U extends Record<string, any>>(
   objA: T,
   objB: U
 ): T & U {
   return Object.assign({}, objA, objB);
-}
+}*/
